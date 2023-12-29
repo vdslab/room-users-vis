f3c682f846cc0c251ca0d1d24d43bb48d58ec647
kinakomoch7 <book-insect14@outlook.jp>
2023-12-30 02:30:03 +0900

    [add]svgのバー実装

--- src/components/charts/rank/RankBar.jsx
+++ src/components/charts/rank/RankBar.jsx
@@ -1,11 +1,13 @@
 import { Text } from "@/components/common/Text";
 import Avatar from "@mui/material/Avatar";
+import { RectSVG } from "./RectSVG";
 
 export const RankBar = () => {
   return (
     <div className="flex">
       <Text message="1" />
       <Avatar />
+      <RectSVG />
     </div>
   );
 };
